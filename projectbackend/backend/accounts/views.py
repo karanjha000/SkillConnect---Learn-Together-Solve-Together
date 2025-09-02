90b6eb7fa75493ee56ae67edb28f6c974e306e9d
karan jha <Karanjhax12@gmail.com>
2025-09-02 20:13:29 +0530

    Views.py edit commit

--- projectbackend/backend/accounts/views.py
+++ projectbackend/backend/accounts/views.py
@@ -13,6 +13,7 @@ def login_user(request):
     password = request.data.get('password')
     try:
         user = User.objects.get(email=email, password=password)
+        print("user logged in")
         return Response({"success": True}, status=status.HTTP_200_OK)
     except User.DoesNotExist:
         return Response({"error": "Invalid email or password"}, status=status.HTTP_401_UNAUTHORIZED)
@@ -24,5 +25,6 @@ def register_user(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
+        print("user created")
         return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
